0ed59af571c09ef62310eac81807c059178102ab
Bruno Ancona <brunoanconasala@gmail.com>
2023-01-28 13:04:37 -0600

    Use ooz for oodle decompression on Linux

--- source/core/Oodle.cpp
+++ source/core/Oodle.cpp
@@ -2,57 +2,61 @@
 
 namespace HAYDEN
 {
+#ifdef _WIN32
     // Decompress using Oodle DLL
     OodLZ_DecompressFunc* OodLZ_Decompress = NULL;
+#else
+    // Decompress using ooz library
+    Kraken_Decompress* KrakenDecompress = NULL;
+#endif
+
     bool oodleInit(const std::string& basePath)
     {
-        std::string oodlePath = basePath.substr(0, basePath.length() - 4) + "oo2core_8_win64.dll";
-
 #ifdef _WIN32
         // Load oodle dll
+        std::string oodlePath = basePath.substr(0, basePath.length() - 4) + "oo2core_8_win64.dll";
         auto oodle = LoadLibraryA(oodlePath.c_str());
         if (!oodle)
             return false;
 
         OodLZ_Decompress = (OodLZ_DecompressFunc*)GetProcAddress(oodle, "OodleLZ_Decompress");
-#else
-        std::error_code ec;
-
-        // Copy oodle to current dir to prevent linoodle errors
-        bool copied = false;
-        if (!fs::equivalent(fs::path(oodlePath).parent_path(), fs::current_path())) {
-            fs::copy(oodlePath, fs::current_path(), fs::copy_options::overwrite_existing, ec);
-            copied = true;
-            if (ec.value() != 0)
-                return false;
-        }
-
-        // Load linoodle library
-        std::string linoodlePath = basePath + "/liblinoodle.so";
-        auto oodle = dlopen(linoodlePath.c_str(), RTLD_LAZY);
-        OodLZ_Decompress = (OodLZ_DecompressFunc*)dlsym(oodle, "OodleLZ_Decompress");
-
-        // Remove oodle dll
-        if (copied)
-            fs::remove(fs::current_path().append("oo2core_8_win64.dll"), ec);
-#endif
-
-        if (oodle == NULL || OodLZ_Decompress == NULL)
+        if (!OodLZ_Decompress)
             return false;
+#else
+        // Load ooz library
+        std::string oozPath = basePath + "/libooz.so";
+        auto ooz = dlopen(oozPath.c_str(), RTLD_LAZY);
+        if (!ooz)
+            return false;
+
+        KrakenDecompress = (Kraken_Decompress*)dlsym(ooz, "Kraken_Decompress");
+        if (!KrakenDecompress)
+            return false;
+#endif
 
         return true;
     }
 
     std::vector<uint8_t> oodleDecompress(std::vector<uint8_t> compressedData, const uint64_t decompressedSize)
     {
+#ifdef _WIN32
         if (OodLZ_Decompress == NULL)
             return std::vector<uint8_t>();
+#else
+        if (KrakenDecompress == NULL)
+            return std::vector<uint8_t>();
+#endif
 
         std::vector<uint8_t> output(decompressedSize + SAFE_SPACE);
         uint64_t outbytes = 0;
 
+#ifdef _WIN32
         // Decompress using Oodle DLL
         outbytes = OodLZ_Decompress(compressedData.data(), compressedData.size(), output.data(), decompressedSize, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+#else
+        // Decompress using ooz library
+        outbytes = KrakenDecompress(compressedData.data(), compressedData.size(), output.data(), decompressedSize);
+#endif
 
         if (outbytes == 0)
         {
--- source/core/Oodle.h
+++ source/core/Oodle.h
@@ -12,9 +12,13 @@
 
 #define SAFE_SPACE 64
 
+#ifdef _WIN32
 typedef int OodLZ_DecompressFunc(
     uint8_t* src_buf, int src_len, uint8_t* dst, size_t dst_size, int fuzz, int crc, int verbose,
     uint8_t* dst_base, size_t e, void* cb, void* cb_ctx, void* scratch, size_t scratch_size, int threadPhase);
+#else
+typedef int Kraken_Decompress(const uint8_t *src, size_t src_len, uint8_t *dst, size_t dst_len);
+#endif
 
 namespace fs = std::filesystem;
 
